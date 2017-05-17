2fdcce893a44772d5fccc8686e35a4865450a069
Anson Huang <Anson.Huang@nxp.com>
2017-05-17 21:59:26 +0800

    Add necessary resources to secure partition for i.MX8QXP
    Add necessary resources to secure partition for protection.
    Also add in functionality to allow for register access
    of some secure-owned peripherals.
    
    These peripherals will still be protected from power or
    clk changes.
    
    Signed-off-by: Anson Huang <Anson.Huang@nxp.com>
    Signed-off-by: Teo Hall <teo.hall@nxp.com>

---
+++ plat/imx/imx8qxp/include/sec_rsrc.h
@@ -0,0 +1,18 @@
+/* Copyright 2017 NXP */
+/* Include file detailing the resource partitioning for ATF */
+
+/* resources that are going to stay in secure partition */
+sc_rsrc_t secure_rsrcs[] = {
+	SC_R_MU_1A,
+	SC_R_A35,
+	SC_R_A35_0,
+	SC_R_A35_1,
+	SC_R_A35_2,
+	SC_R_A35_3,
+	SC_R_GIC,
+};
+
+/* resources that have register access for non-secure domain */
+sc_rsrc_t ns_access_allowed[] = {
+	SC_R_GIC,
+};
