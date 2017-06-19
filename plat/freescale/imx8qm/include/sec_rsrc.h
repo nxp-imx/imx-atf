f14367e5c33a85a6b6a33f321f57d7c819dda45e
Ye Li <ye.li@nxp.com>
2017-06-19 03:12:09 -0500

    imx8qm/qxp: Modify the memory regions allocation to NS partition
    Change to search the ATF owned memory regions and assign them to non-secure
    OS partition. Not allocate new memory region for each one.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>

--- plat/freescale/imx8qm/include/sec_rsrc.h
+++ plat/freescale/imx8qm/include/sec_rsrc.h
@@ -23,19 +23,3 @@ sc_rsrc_t ns_access_allowed[] = {
   SC_R_GIC_SMMU,
   SC_R_CCI
 };
-
-struct mem_region {
-	unsigned long start;
-	unsigned long end;
-};
-
-struct mem_region ns_mem_region[] = {
-	{0x000000000, 0x01BFFFFFF},
-	{0x034000000, 0x037FFFFFF},
-	{0x038000000, 0x03BFFFFFF},
-	{0x060000000, 0x06FFFFFFF},
-	{0x070000000, 0x07FFFFFFF},
-	{0x080000000, 0x0FFFFFFFF},
-	{0x400000000, 0x43FFFFFFF},
-	{0x880000000, 0xFFFFFFFFF},
-};
