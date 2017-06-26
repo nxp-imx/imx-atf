f8c2813336f29d0a31bf70ac1c96af6344e49d68
Bai Ping <ping.bai@nxp.com>
2017-06-26 10:47:47 +0800

    i.mx8mq: Add basic support for i.mx8mq
    Add basic support for i.MX8MQ.
    1. SMP support is ok.
    2. basic suspend/resume support is ok.
    
    Signed-off-by: Bai Ping <ping.bai@nxp.com>

---
+++ plat/imx/imx8mq/include/fsl_sip.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2017 NXP
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef __SOC_FSL_SIP_H
+#define __SOC_FSL_SIP_H
+
+#define FSL_SIP_GPC			0xC2000000
+#define FSL_SIP_CONFIG_GPC_MASK		0x00
+#define FSL_SIP_CONFIG_GPC_UNMASK	0x01
+#define FSL_SIP_CONFIG_GPC_SET_WAKE	0x02
+#define FSL_SIP_CONFIG_GPC_PM_DOMAIN	0x03
+
+#endif
---
+++ plat/imx/imx8mq/platform.mk
@@ -0,0 +1,28 @@
+PLAT_INCLUDES		:=	-Iplat/imx/imx8mq/include		\
+				-Iplat/imx/common/include		\
+
+PLAT_GIC_SOURCES	:=	drivers/arm/gic/v3/gicv3_helpers.c	\
+				drivers/arm/gic/v3/gicv3_main.c		\
+				drivers/arm/gic/common/gic_common.c	\
+				plat/common/plat_gicv3.c		\
+				plat/imx/common/plat_imx8_gic.c
+
+BL31_SOURCES		+=	plat/imx/common/imx8_helpers.S		\
+				plat/imx/common/mxcuart_console.S	\
+				plat/imx/imx8mq/imx8m_bl31_setup.c	\
+				plat/imx/imx8mq/gpc.c			\
+				plat/imx/imx8mq/imx8m_psci.c		\
+				plat/imx/imx8mq/sip_svc.c		\
+				plat/imx/common/imx8_topology.c		\
+				plat/common/plat_psci_common.c		\
+				lib/xlat_tables/aarch64/xlat_tables.c	\
+				lib/xlat_tables/xlat_tables_common.c	\
+				lib/cpus/aarch64/cortex_a53.S		\
+				drivers/console/aarch64/console.S	\
+				${PLAT_GIC_SOURCES}			\
+
+ENABLE_PLAT_COMPAT	:=	0
+USE_COHERENT_MEM	:=	0
+MULTI_CONSOLE_API	:=	1
+RESET_TO_BL31		:=	1
+ERROR_DEPRECATED	:=	1
