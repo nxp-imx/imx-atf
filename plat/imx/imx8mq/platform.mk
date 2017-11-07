5ca8d95b1797f2e169deec5029bf3ac1c41c5c85
Aymen Sghaier <aymen.sghaier@nxp.com>
2017-11-07 15:28:30 +0100

    imx8mq: Add rdc support
      Enable the RDC driver for i.MX8MQ platform with a default settings
     as an example.
    
    Signed-off-by: Aymen Sghaier <aymen.sghaier@nxp.com>

--- plat/imx/imx8mq/platform.mk
+++ plat/imx/imx8mq/platform.mk
@@ -16,6 +16,7 @@ BL31_SOURCES		+=	plat/imx/common/imx8_helpers.S		\
 				plat/imx/imx8mq/ddrc.c			\
 				plat/imx/imx8mq/imx8m_psci.c		\
 				plat/imx/imx8mq/imx_csu.c		\
+				plat/imx/imx8mq/imx_rdc.c		\
 				plat/imx/common/imx8_topology.c		\
 				plat/common/plat_psci_common.c		\
 				lib/xlat_tables/aarch64/xlat_tables.c	\
