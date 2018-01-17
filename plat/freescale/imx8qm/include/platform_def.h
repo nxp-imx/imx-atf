01bde530707d222ae3b70c48bb97546e8513bac7
Anson Huang <Anson.Huang@nxp.com>
2018-01-17 15:43:51 +0800

    imx8qm/imx8qxp: enlarge MAX_XLAT_TABLES to make debug version work
    In worse case, MAX_XLAT_TABLES needs to be equal to
    MAX_MMAP_REGIONS, enlarge MAX_XLAT_TABLES to make debug
    version ATF can boot up, otherwise, it may fail at below:
    
    ASSERT: lib/xlat_tables/xlat_tables_common.c <362>
    
    Signed-off-by: Anson Huang <Anson.Huang@nxp.com>

--- plat/freescale/imx8qm/include/platform_def.h
+++ plat/freescale/imx8qm/include/platform_def.h
@@ -71,7 +71,7 @@
 #define PLAT_VIRT_ADDR_SPACE_SIZE	(1ull << 32)
 #define PLAT_PHY_ADDR_SPACE_SIZE	(1ull << 32)
 
-#define MAX_XLAT_TABLES			4
+#define MAX_XLAT_TABLES			8
 #define MAX_MMAP_REGIONS		8
 
 #define IMX_BOOT_UART_BASE		0x5a060000
