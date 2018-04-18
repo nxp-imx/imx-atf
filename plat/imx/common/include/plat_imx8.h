02871d54a6beda8bb0c09e43aacfd138bd5fe35a
Bai Ping <ping.bai@nxp.com>
2018-04-18 16:16:48 +0800

    plat: imx: add platfrom gicv3 save/restore
    The GICv3 save/restore is necessary when GIC is going
    to power off. the save/restore is common for all imx8
    platform, so make it common for imx8.
    
    Signed-off-by: Bai Ping <ping.bai@nxp.com>

--- plat/imx/common/include/plat_imx8.h
+++ plat/imx/common/include/plat_imx8.h
@@ -31,6 +31,14 @@
 #ifndef __PLAT_IMX8_H__
 #define __PLAT_IMX8_H__
 
+#include <gicv3.h>
+
+/* gicv3 context save */
+struct plat_gic_ctx {
+	gicv3_redist_ctx_t rdist_ctx;
+	gicv3_dist_ctx_t dist_ctx;
+};
+
 unsigned int plat_calc_core_pos(u_register_t mpidr);
 void imx_mailbox_init(uintptr_t base_addr);
 void plat_gic_driver_init(void);
@@ -38,5 +46,7 @@ void plat_gic_init(void);
 void plat_gic_cpuif_enable(void);
 void plat_gic_cpuif_disable(void);
 void plat_gic_pcpu_init(void);
+void plat_gic_save(unsigned int proc_num, struct plat_gic_ctx *ctx);
+void plat_gic_restore(unsigned int proc_num, struct plat_gic_ctx *ctx);
 
 #endif /*__PLAT_IMX8_H__ */
