ba66c22e0f79e324902090ca8633776fb321a910
Silvano di Ninno <silvano.dininno@nxp.com>
2018-05-17 17:32:10 +0200

    MLK-18343-2: plat: imx8mm: add support for RDC and CSU
    move CSU and RDC driver to common/i.mx8m folder
    and enable the driver for i.mx8mm
    
    Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>
    Reviewed-by: Aymen Sghaier <aymen.sghaier@nxp.com>
    (cherry picked from commit 7dd1b741efdc60ca6b5eaafc9d863744beb7064e)
    
    Conflicts:

--- plat/imx/imx8mq/platform.mk
+++ plat/imx/imx8mq/platform.mk
@@ -22,8 +22,8 @@ BL31_SOURCES		+=	plat/imx/common/imx8_helpers.S		\
 				plat/imx/common/imx8_sip_svc.c		\
 				plat/imx/common/misc.c			\
 				plat/imx/imx8mq/imx8mq_psci.c		\
-				plat/imx/imx8mq/imx_csu.c		\
-				plat/imx/imx8mq/imx_rdc.c		\
+				plat/imx/common/imx8m/imx_csu.c		\
+				plat/imx/common/imx8m/imx_rdc.c		\
 				plat/imx/common/imx8_topology.c		\
 				plat/common/plat_psci_common.c		\
 				lib/xlat_tables/aarch64/xlat_tables.c	\
