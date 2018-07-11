dfa6a19c2d44de65e6c03b73124f8b0d4ab81eac
Bai Ping <ping.bai@nxp.com>
2018-07-11 16:23:08 +0800

    plat: imx: refact the dram retention flow on imx8mm
    All the DRAM timing related config is saved by SPL in OCRAM_S,
    so no need to do save for these configs in ATF anymore.
    
    Signed-off-by: Bai Ping <ping.bai@nxp.com>
    (cherry picked from commit f0893b1f52303ba5604480db71efe605161666eb)

--- plat/imx/common/imx8m/dram.c
+++ plat/imx/common/imx8m/dram.c
@@ -9,52 +9,55 @@
 #include <dram.h>
 #include <mmio.h>
 
-static struct dram_info *dram_info;
-
-void dram_cfg_save(struct dram_cfg_param *params, int params_num)
-{
-	for (int idx = 0;  idx < params_num; idx++) {
-		params->cfg = mmio_read_32(params->reg);
-		params++;
-	}
-}
-
-void dram_cfg_restore(struct dram_cfg_param *params, int params_num)
-{
-	for (int idx = 0; idx < params_num; idx++) {
-		mmio_write_32(params->reg, params->cfg);
-		params++;
-	}
-}
+static struct dram_info dram_info;
 
 /* restore the ddrc config */
 void dram_umctl2_init(void)
 {
-	struct dram_cfg_param *params = dram_info->ddrc_cfg;
-	int params_num = dram_info->ddrc_cfg_num;
+	struct dram_timing_info *timing = dram_info.timing_info;
+	struct dram_cfg_param *ddrc_cfg = timing->ddrc_cfg;
+	int num = timing->ddrc_cfg_num;
 
-	dram_cfg_restore(params, params_num);
-	mmio_write_32(DDRC_DBG1(0), 0x1);
-	mmio_write_32(DDRC_DFIMISC(0), 0x11);
-	mmio_write_32(DDRC_DFIUPD0(0),0xE0400018); 
+	for (int i = 0; i < num; i++) {
+		mmio_write_32(ddrc_cfg->reg, ddrc_cfg->val);
+		ddrc_cfg++;
+	}
+
+	/* set the default fsp to P0 */
+	mmio_write_32(DDRC_MSTR2(0), 0x0);
 }
 
 /* resotre the dram phy config */
 void dram_phy_init(void)
 {
-	struct dram_cfg_param *ddrphy_params = dram_info->ddrphy_cfg;
-	int params_num = dram_info->ddrphy_cfg_num;
+	struct dram_timing_info *timing = dram_info.timing_info;
+	struct dram_cfg_param *ddrphy_cfg = timing->ddrphy_cfg;
+	int num = timing->ddrphy_cfg_num;
 
-	/* retore the phy config */
-	mmio_write_32(DDRPHY_REG(0xd0000), 0x0);
-	dram_cfg_restore(ddrphy_params, params_num);
-	mmio_write_32(DDRPHY_REG(0xd0000), 0x1);
+	/* restore the phy init config */
+	for (int i = 0; i < num; i++) {
+		dwc_ddrphy_apb_wr(ddrphy_cfg->reg, ddrphy_cfg->val);
+		ddrphy_cfg++;
+	}
+
+	/* restore the ddr phy csr */
+	num = timing->ddrphy_trained_csr_num;
+	ddrphy_cfg = timing->ddrphy_trained_csr;
+	for (int i = 0; i < num; i++) {
+		dwc_ddrphy_apb_wr(ddrphy_cfg->reg, ddrphy_cfg->val);
+		ddrphy_cfg++;
+	}
 
 	/* load the PIE image */
-	ddrphy_load_pie_image();
+	num = timing->ddrphy_pie_num;
+	ddrphy_cfg = timing->ddrphy_pie;
+	for (int i = 0; i < num; i++) {
+		dwc_ddrphy_apb_wr(ddrphy_cfg->reg, ddrphy_cfg->val);
+		ddrphy_cfg++;
+	}
 }
 
-void dram_info_init(void)
+void dram_info_init(unsigned long dram_timing_base)
 {
 	uint32_t current_fsp, ddr_type;
 
@@ -62,8 +65,7 @@ void dram_info_init(void)
 	ddr_type = mmio_read_32(DDRC_MSTR(0)) & DDR_TYPE_MASK;
 
 	if (ddr_type == DDRC_LPDDR4) {
-		dram_info = &imx8m_lpddr4_dram_info;
-		dram_info->dram_type = ddr_type;
+		dram_info.dram_type = ddr_type;
 	} else {
 		/* TODO DDR4 support will be added later */
 		return;
@@ -72,30 +74,26 @@ void dram_info_init(void)
 	/* init the boot_fsp & current_fsp */
 	current_fsp = mmio_read_32(DDRC_DFIMISC(0));
 	current_fsp = (current_fsp >> 8) & 0xf;
-	dram_info->boot_fsp = current_fsp;
-	dram_info->current_fsp = current_fsp;
+	dram_info.boot_fsp = current_fsp;
+	dram_info.current_fsp = current_fsp;
 
-	/* save the ddrc config */
-	dram_cfg_save(dram_info->ddrc_cfg, dram_info->ddrc_cfg_num);
-
-	/* save dram phy config */
-	mmio_write_32(DDRPHY_REG(0xd0000), 0x0);
-	mmio_write_32(DDRPHY_REG(0xc0080), 0x3);
-	dram_cfg_save(dram_info->ddrphy_cfg, dram_info->ddrphy_cfg_num);
-	mmio_write_32(DDRPHY_REG(0xc0080), 0x0);
-	mmio_write_32(DDRPHY_REG(0xd0000), 0x1);
+	/*
+	 * No need to do save for ddrc and phy config register,
+	 * we have done it in SPL stage and save in memory
+	 */
+	dram_info.timing_info = (struct dram_timing_info *)dram_timing_base;
 }
 
 void dram_enter_retention(void)
 {
 	/* TODO add the ddr4 support in the furture */
-	if (dram_info->dram_type == DDRC_LPDDR4)
+	if (dram_info.dram_type == DDRC_LPDDR4)
 		lpddr4_enter_retention();
 }
 
 void dram_exit_retention(void)
 {
 	/* TODO add the ddr4 support in the furture */
-	if (dram_info->dram_type == DDRC_LPDDR4)
+	if (dram_info.dram_type == DDRC_LPDDR4)
 		lpddr4_exit_retention();
 }
--- plat/imx/common/include/dram.h
+++ plat/imx/common/include/dram.h
@@ -17,24 +17,36 @@
 /* reg & config param */
 struct dram_cfg_param {
 	unsigned int reg;
-	unsigned int cfg;
+	unsigned int val;
+};
+
+struct dram_timing_info {
+	/* umctl2 config */
+	struct dram_cfg_param *ddrc_cfg;
+	unsigned int ddrc_cfg_num;
+	/* ddrphy config */
+	struct dram_cfg_param *ddrphy_cfg;
+	unsigned int ddrphy_cfg_num;
+	/* ddr fsp train info */
+	struct dram_fsp_msg *fsp_msg;
+	unsigned int fsp_msg_num;
+	/* ddr phy trained CSR */
+	struct dram_cfg_param *ddrphy_trained_csr;
+	unsigned int ddrphy_trained_csr_num;
+	/* ddr phy PIE */
+	struct dram_cfg_param *ddrphy_pie;
+	unsigned int ddrphy_pie_num;
 };
 
 struct dram_info {
 	int dram_type;
 	int current_fsp;
 	int boot_fsp;
-	struct dram_cfg_param *ddrc_cfg;
-	unsigned int ddrc_cfg_num;
-	struct dram_cfg_param *ddrphy_cfg;
-	unsigned int ddrphy_cfg_num;
+	struct dram_timing_info *timing_info;
 };
 
-/* lpddr4 ddrc and phy config info */
-extern struct dram_info imx8m_lpddr4_dram_info;
-
 void ddrphy_load_pie_image(void);
-void dram_info_init(void);
+void dram_info_init(unsigned long dram_timing_base);
 void lpddr4_enter_retention(void);
 void lpddr4_exit_retention(void);
 void dram_umctl2_init(void);
