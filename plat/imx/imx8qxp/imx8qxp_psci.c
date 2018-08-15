11248af8bd01fe232c4f3c8f510d157b1d210dca
Anson Huang <Anson.Huang@nxp.com>
2018-08-15 12:45:07 +0800

    imx8qxp: put DRC into OFF mode when Linux suspend
    SCFW has supported DDR retention, so now DRC can be
    into OFF mode instead of STBY when Linux suspend.
    
    Signed-off-by: Anson Huang <Anson.Huang@nxp.com>

--- plat/imx/imx8qxp/imx8qxp_psci.c
+++ plat/imx/imx8qxp/imx8qxp_psci.c
@@ -275,7 +275,7 @@ int plat_setup_psci_ops(uintptr_t sec_entrypoint,
 	sc_pm_req_low_power_mode(ipc_handle, SC_R_A35, SC_PM_PW_MODE_OFF);
 
 	/* Request RUN and LP modes for DDR, system interconnect etc. */
-	sc_pm_req_sys_if_power_mode(ipc_handle, SC_R_A35, SC_PM_SYS_IF_DDR, SC_PM_PW_MODE_ON, SC_PM_PW_MODE_STBY);
+	sc_pm_req_sys_if_power_mode(ipc_handle, SC_R_A35, SC_PM_SYS_IF_DDR, SC_PM_PW_MODE_ON, SC_PM_PW_MODE_OFF);
 	sc_pm_req_sys_if_power_mode(ipc_handle, SC_R_A35, SC_PM_SYS_IF_MU, SC_PM_PW_MODE_ON, SC_PM_PW_MODE_OFF);
 	sc_pm_req_sys_if_power_mode(ipc_handle, SC_R_A35, SC_PM_SYS_IF_INTERCONNECT, SC_PM_PW_MODE_ON, SC_PM_PW_MODE_OFF);
 
