3679caa0184bf14d7f3e61c425e076587dca6e6b
Ruchika Gupta <ruchika.gupta@nxp.com>
2018-09-05 14:24:50 +0530

    nxp: Add TF-A support for LS1012A
    This patch introduces TF-A support for NXP's ls1012a platform.
    
    TF-A on ls1012a platform enables the following features:
    	- Boards Supported - FRDM, FRWY, QDS, RDB
    	- Boot from QSPI flash
    	- Trusted Boot
    	- TF-A can boot bl33 which runs in el2 of non-secure world.
    	- TF-A boot OPTee OS.
    	- Support PSCI
    
    Signed-off-by: Alison Wang <alison.wang@nxp.com>
    Signed-off-by: Pankaj Gupta <pankaj.gupta@nxp.com>
    Signed-off-by: York Sun <york.sun@nxp.com>
    Signed-off-by: Ruchika Gupta <ruchika.gupta@nxp.com>
    Signed-off-by: rocket <rod.dorris@nxp.com>
    Signed-off-by: Biwen Li <biwen.li@nxp.com>

---
+++ plat/nxp/soc-ls1012a/ls1012afrdm/ddr_init.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2018, 2021 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <common/debug.h>
+#include <errno.h>
+#include <fsl_mmdc.h>
+#include <plat_common.h>
+#include <platform_def.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+long long init_ddr(void)
+{
+	static const struct fsl_mmdc_info mparam = {
+		.mdctl = 0x04180000,
+		.mdpdc = 0x00030035,
+		.mdotc = 0x12554000,
+		.mdcfg0 = 0xbabf7954,
+		.mdcfg1 = 0xdb328f64,
+		.mdcfg2 = 0x01ff00db,
+		.mdmisc = 0x00001680,
+		.mdref = 0x0f3c8000,
+		.mdrwd = 0x00002000,
+		.mdor = 0x00bf1023,
+		.mdasp = 0x0000003f,
+		.mpodtctrl = 0x0000022a,
+		.mpzqhwctrl = 0xa1390003,
+	};
+
+	mmdc_init(&mparam, NXP_DDR_ADDR);
+	NOTICE("DDR Init Done\n");
+
+	return NXP_DRAM0_SIZE;
+}
