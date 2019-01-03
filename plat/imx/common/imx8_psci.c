87fbdbd3a730c54f4127827339def97f1802a4dc
Anson Huang <Anson.Huang@nxp.com>
2019-01-03 13:30:32 +0800

    Add NXP's i.MX8QXP power management support
    This patch adds i.MX8QXP power management support including below
    features:
    
    - cpu hot-plug;
    - system suspend, including all Cortex-A resources power OFF support,
      necessary modules save/restore like GIC, MU etc..
    - cpu-idle with cpu/cluster ON/OFF according to power state;
    - wakeup source runtime setup according to Linux kernel's setting,
      IRQSTEER or SCU or GIC;
    
    Signed-off-by: Anson Huang <Anson.Huang@nxp.com>

--- plat/imx/common/imx8_psci.c
+++ plat/imx/common/imx8_psci.c
@@ -27,20 +27,41 @@ void __dead2 imx_system_reset(void)
 	panic();
 }
 
+#define CORE_PWR_STATE(state) ((state)->pwr_domain_state[MPIDR_AFFLVL0])
+#define CLUSTER_PWR_STATE(state) ((state)->pwr_domain_state[MPIDR_AFFLVL1])
+#define SYSTEM_PWR_STATE(state) ((state)->pwr_domain_state[PLAT_MAX_PWR_LVL])
+
 int imx_validate_power_state(unsigned int power_state,
 			 psci_power_state_t *req_state)
 {
-	/* TODO */
-	return PSCI_E_INVALID_PARAMS;
+	int pwr_lvl = psci_get_pstate_pwrlvl(power_state);
+	int pwr_type = psci_get_pstate_type(power_state);
+	int state_id = psci_get_pstate_id(power_state);
+
+	if (pwr_lvl > PLAT_MAX_PWR_LVL)
+		return PSCI_E_INVALID_PARAMS;
+
+	if (pwr_type == PSTATE_TYPE_POWERDOWN) {
+		CORE_PWR_STATE(req_state) = PLAT_MAX_OFF_STATE;
+		if (!state_id)
+			CLUSTER_PWR_STATE(req_state) = PLAT_MAX_RET_STATE;
+		else
+			CLUSTER_PWR_STATE(req_state) = PLAT_MAX_OFF_STATE;
+	}
+
+	return PSCI_E_SUCCESS;
 }
 
 void imx_get_sys_suspend_power_state(psci_power_state_t *req_state)
 {
 	unsigned int i;
 
-	/* CPU & cluster off, system in retention */
-	for (i = MPIDR_AFFLVL0; i < PLAT_MAX_PWR_LVL; i++)
-		req_state->pwr_domain_state[i] = PLAT_MAX_OFF_STATE;
-	req_state->pwr_domain_state[PLAT_MAX_PWR_LVL] = PLAT_MAX_RET_STATE;
+	for (i = IMX_PWR_LVL0; i <= PLAT_MAX_PWR_LVL; i++)
+		req_state->pwr_domain_state[i] = PLAT_MAX_RET_STATE;
 }
 
+void __dead2 imx_pwr_domain_pwr_down_wfi(const psci_power_state_t *target_state)
+{
+	while (1)
+		wfi();
+}
--- plat/imx/common/include/plat_imx8.h
+++ plat/imx/common/include/plat_imx8.h
@@ -10,6 +10,11 @@
 #include <gicv3.h>
 #include <psci.h>
 
+struct plat_gic_ctx {
+	gicv3_redist_ctx_t rdist_ctx[PLATFORM_CORE_COUNT];
+	gicv3_dist_ctx_t dist_ctx;
+};
+
 unsigned int plat_calc_core_pos(uint64_t mpidr);
 void imx_mailbox_init(uintptr_t base_addr);
 void plat_gic_driver_init(void);
@@ -17,10 +22,14 @@ void plat_gic_init(void);
 void plat_gic_cpuif_enable(void);
 void plat_gic_cpuif_disable(void);
 void plat_gic_pcpu_init(void);
+void plat_gic_save(unsigned int proc_num, struct plat_gic_ctx *ctx);
+void plat_gic_restore(unsigned int proc_num, struct plat_gic_ctx *ctx);
 
 void __dead2 imx_system_off(void);
 void __dead2 imx_system_reset(void);
 int imx_validate_power_state(unsigned int power_state,
 			psci_power_state_t *req_state);
 void imx_get_sys_suspend_power_state(psci_power_state_t *req_state);
+bool imx_is_wakeup_src_irqsteer(void);
+void __dead2 imx_pwr_domain_pwr_down_wfi(const psci_power_state_t *target_state);
 #endif /*__PLAT_IMX8_H__ */
