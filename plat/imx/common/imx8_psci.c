b8e79a7456a467a077c6c0a5b8999816c5d977ad
Anson Huang <Anson.Huang@nxp.com>
2019-01-03 17:27:51 +0800

    Add NXP's SoCs partition reboot support.
    For NXP's SoCs with system controller inside, partition reboot
    is supported in SCFW, change the system reset API from board reset
    to partition reboot, so each partition can reboot independently.
    
    Signed-off-by: Anson Huang <Anson.Huang@nxp.com>

--- plat/imx/common/imx8_psci.c
+++ plat/imx/common/imx8_psci.c
@@ -23,7 +23,7 @@ void __dead2 imx_system_off(void)
 
 void __dead2 imx_system_reset(void)
 {
-	sc_pm_reset(ipc_handle, SC_PM_RESET_TYPE_BOARD);
+	sc_pm_reboot(ipc_handle, SC_PM_RESET_TYPE_COLD);
 	wfi();
 	ERROR("system reset failed.\n");
 	panic();
