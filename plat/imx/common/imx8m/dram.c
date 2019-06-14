227fcb2f4e85588ed872d7d09844bf91163018b3
Ji Luo <ji.luo@nxp.com>
2019-06-14 13:15:12 +0800

    Revert "MLK-21399 plat: imx8mq: gpc: Workaround for ERR11171"
    This reverts commit 0e91ff59720d07563e2bdfc83e610dd2005bc56b.
    
    Revert this patch for Android to reduce the image size for AIY.
    
    Change-Id: I4a450f0d364849d722dec5c5d66a8186f146d970

--- plat/imx/common/imx8m/dram.c
+++ plat/imx/common/imx8m/dram.c
@@ -255,17 +255,16 @@ int dram_dvfs_handler(uint32_t smc_fid,
 
 		/* make sure all the core in WFE */
 		online_cores &= ~(0x1 << (cpu_id * 8));
-#if defined(PLAT_IMX8M)
-		for (int i = 0; i < 4; i++) {
-			if (i != cpu_id && online_cores & (1 << (i * 8))) {
-				imx_gpc_core_wake(1 << i);
-			}
-		}
-#endif
 		while (1) {
+#if defined(PLAT_IMX8M)
+			mmio_write_32(0x30340004, mmio_read_32(0x30340004) | (1 << 12));
+#endif
 			if (online_cores == wfe_done)
 				break;
 		}
+#if defined(PLAT_IMX8M)
+		mmio_write_32(0x30340004, mmio_read_32(0x30340004) & ~(1 << 12));
+#endif
 
 		/* flush the L1/L2 cache */
 		dcsw_op_all(DCCSW);
