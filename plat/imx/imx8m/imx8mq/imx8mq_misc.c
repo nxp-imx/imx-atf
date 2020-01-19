97c923b2342a834ed82c2f95c17a0e37d943d902
Jacky Bai <ping.bai@nxp.com>
2020-01-19 13:35:13 +0800

    plat: imx8mq: Add the NOC sip handler on imx8mq
    Add the NOC QoS setting SiP handler on imx8mq.
    
    Signed-off-by: Jacky Bai <ping.bai@nxp.com>
    Change-Id: I1f377090c36ec6ed135c44e8bf2ad771463363fb

--- plat/imx/imx8m/imx8mq/imx8mq_misc.c
+++ plat/imx/imx8m/imx8mq/imx8mq_misc.c
@@ -44,3 +44,44 @@ int imx_src_handler(uint32_t smc_fid, u_register_t x1, u_register_t x2,
 
 	return 0;
 }
+
+int imx_noc_handler(uint32_t smc_fid, u_register_t x1, u_register_t x2,
+		    u_register_t x3)
+{
+	if (IMX_SIP_NOC_LCDIF == x1) {
+		/* config NOC for VPU */
+		mmio_write_32(IMX_NOC_BASE + 0x108, 0x34);
+		mmio_write_32(IMX_NOC_BASE + 0x10c, 0x1);
+		mmio_write_32(IMX_NOC_BASE + 0x110, 0x500);
+		mmio_write_32(IMX_NOC_BASE + 0x114, 0x30);
+		/* config NOC for CPU */
+		mmio_write_32(IMX_NOC_BASE + 0x188, 0x34);
+		mmio_write_32(IMX_NOC_BASE + 0x18c, 0x1);
+		mmio_write_32(IMX_NOC_BASE + 0x190, 0x500);
+		mmio_write_32(IMX_NOC_BASE + 0x194, 0x30);
+	} else if (IMX_SIP_NOC_PRIORITY == x1) {
+		switch(x2) {
+		case NOC_GPU_PRIORITY:
+			mmio_write_32(IMX_NOC_BASE + 0x008, x3);
+			break;
+		case NOC_DCSS_PRIORITY:
+			mmio_write_32(IMX_NOC_BASE + 0x088, x3);
+			break;
+		case NOC_VPU_PRIORITY:
+			mmio_write_32(IMX_NOC_BASE + 0x108, x3);
+			break;
+		case NOC_CPU_PRIORITY:
+			mmio_write_32(IMX_NOC_BASE + 0x188, x3);
+			break;
+		case NOC_MIX_PRIORITY:
+			mmio_write_32(IMX_NOC_BASE + 0x288, x3);
+			break;
+		default:
+			return SMC_UNK;
+		};
+	} else {
+		return SMC_UNK;
+	}
+
+	return 0;
+}
