392b16c53e37a1beb001af2a2a75e90f20b749fc
Jacky Bai <ping.bai@nxp.com>
2020-02-13 11:27:34 +0800

    plat: imx8m: Replace the magic number rdc/csu index with enum type
    Replace the magic number index with enum type to make RDC/CSU config
    more clear for user.
    
    Signed-off-by: Jacky Bai <ping.bai@nxp.com>

--- plat/imx/imx8m/imx8mn/imx8mn_bl31_setup.c
+++ plat/imx/imx8m/imx8mn/imx8mn_bl31_setup.c
@@ -45,11 +45,11 @@ static const struct aipstz_cfg aipstz[] = {
 
 static const struct imx_rdc_cfg rdc[] = {
 	/* Master domain assignment */
-	RDC_MDAn(0x1, DID1),
+	RDC_MDAn(RDC_MDA_M7, DID1),
 
 	/* peripherals domain permission */
-	RDC_PDAPn(70, D1R | D1W),
-	RDC_PDAPn(105, D0R | D0W),
+	RDC_PDAPn(RDC_PDAP_UART4, D1R | D1W),
+	RDC_PDAPn(RDC_PDAP_UART2, D0R | D0W),
 
 	/* memory region */
 	RDC_MEM_REGIONn(16, 0x0, 0x0, 0xff),
@@ -62,8 +62,8 @@ static const struct imx_rdc_cfg rdc[] = {
 
 static const struct imx_csu_cfg csu_cfg[] = {
 	/* peripherals csl setting */
-	CSU_CSLx(118, CSU_SEC_LEVEL_2, UNLOCKED),
-	CSU_CSLx(119, CSU_SEC_LEVEL_2, UNLOCKED),
+	CSU_CSLx(CSU_CSL_OCRAM, CSU_SEC_LEVEL_2, UNLOCKED),
+	CSU_CSLx(CSU_CSL_OCRAM_S, CSU_SEC_LEVEL_2, UNLOCKED),
 
 	/* master HP0~1 */
 
