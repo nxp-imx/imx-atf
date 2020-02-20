24893922167c2b67e89ca2b9ae9695988c5ab81c
Ji Luo <ji.luo@nxp.com>
2020-02-20 11:53:50 +0800

    MA-16431 plat: imx8mp: enlarge maximum number of memory regions and tables
    Enlarge the maximum number of memory map regions and translation tables
    for imx8mp.
    
    Test: builds.
    
    Change-Id: I1d9bd56cd13bc003cb509624972e66bce84d0434
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

--- plat/imx/imx8mp/include/platform_def.h
+++ plat/imx/imx8mp/include/platform_def.h
@@ -48,8 +48,8 @@
 #define PLAT_VIRT_ADDR_SPACE_SIZE	(1ull << 32)
 #define PLAT_PHY_ADDR_SPACE_SIZE	(1ull << 32)
 
-#define MAX_XLAT_TABLES			4
-#define MAX_MMAP_REGIONS		13
+#define MAX_XLAT_TABLES			8
+#define MAX_MMAP_REGIONS		16
 
 #define HAB_RVT_BASE			0x00000900 /* HAB_RVT for i.MX8MM */
 
