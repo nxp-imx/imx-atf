ce34cd55ab2e0adaa836906497cfebd5a9d649b0
Jacky Bai <ping.bai@nxp.com>
2020-04-16 14:48:06 +0800

    MLK-23798 plat: imx8mp: Update the noc power down flow of imx8mp
    When system entering DSM mode, the main NOC wrapper only need to
    be on if any of the MIX with ADB400 port is on, so update the flow
    for this.
    
    Signed-off-by: Jacky Bai <ping.bai@nxp.com>
    Reviewed-by: Anson Huang <Anson.Huang@nxp.com>

--- plat/imx/imx8m/gpc_common.c
+++ plat/imx/imx8m/gpc_common.c
@@ -243,7 +243,7 @@ void imx_noc_slot_config(bool pdn)
 		mmio_setbits_32(IMX_GPC_BASE + SLTx_CFG(1), NOC_PDN_SLT_CTRL);
 		mmio_setbits_32(IMX_GPC_BASE + SLTx_CFG(2), NOC_PUP_SLT_CTRL);
 		/* clear a53's PDN ack, use NOC's PDN ack */
-		mmio_clrsetbits_32(IMX_GPC_BASE + PGC_ACK_SEL_A53, 0xffff, NOC_PGC_PDN_ACK);
+		mmio_clrsetbits_32(IMX_GPC_BASE + PGC_ACK_SEL_A53, A53_PLAT_PDN_ACK, NOC_PGC_PDN_ACK);
 		mmio_setbits_32(IMX_GPC_BASE + NOC_PGC_PCR, 0x1);
 	} else {
 		mmio_clrbits_32(IMX_GPC_BASE + SLTx_CFG(1), NOC_PDN_SLT_CTRL);
