0beb6e1ee4ff617423cfd207aae6ac7b71d33673
Jacky Bai <ping.bai@nxp.com>
2020-05-08 17:37:24 +0800

    MLK-23821-04 plat: imx8m: Fix the rank to rank issue
    update umctl2's setting based on phy training CDD value
    to workaround the rank-to-rank space issue.
    
    Signed-off-by: Jacky Bai <ping.bai@nxp.com>
    Reviewed-by: Anson Huang <anson.huang@nxp.com>
    Change-Id: I0fab18cdc378fda760daa0f89c4dd84eb46f7e11

--- plat/imx/imx8m/ddr/dram_retention.c
+++ plat/imx/imx8m/ddr/dram_retention.c
@@ -23,6 +23,32 @@
 
 #define DBGCAM_EMPTY		0x36000000
 
+static void rank_setting_update(void)
+{
+	uint32_t i, offset;
+	uint32_t pstate_num = dram_info.num_fsp;
+
+	for (i = 0; i < pstate_num; i++) {
+		offset = i ? (i + 1) * 0x1000 : 0;
+		if (dram_info.dram_type == DDRC_LPDDR4) {
+			mmio_write_32(DDRC_DRAMTMG2(0) + offset,
+				dram_info.rank_setting[i][0]);
+		} else {
+			mmio_write_32(DDRC_DRAMTMG2(0) + offset,
+				dram_info.rank_setting[i][0]);
+			mmio_write_32(DDRC_DRAMTMG9(0) + offset,
+				dram_info.rank_setting[i][1]);
+		}
+#if !defined(PLAT_imx8mq)
+		mmio_write_32(DDRC_RANKCTL(0) + offset,
+			dram_info.rank_setting[i][2]);
+#endif
+	}
+#if defined(PLAT_imx8mq)
+		mmio_write_32(DDRC_RANKCTL(0), dram_info.rank_setting[0][2]);
+#endif
+}
+
 void dram_enter_retention(void)
 {
 	/* Wait DBGCAM to be empty */
@@ -154,6 +180,9 @@ void dram_exit_retention(void)
 
 	/* dram phy re-init */
 	dram_phy_init(dram_info.timing_info);
+
+	/* workaround for rank-to-rank issue */
+	rank_setting_update();
 
 	/* DWC_DDRPHYA_APBONLY0_MicroContMuxSel */
 	dwc_ddrphy_apb_wr(0xd0000, 0x0);
