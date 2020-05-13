65120140423b51b14384ebe873544faed1c23cb2
Silvano di Ninno <silvano.dininno@nxp.com>
2020-05-14 01:38:32 +0200

    MLK-23930-1 plat: imx8mq: cleanup csu and rdc implementation
    Align CSU CSL defines with the rest of the imx8m family
    Compile csu and rdc drivers.
    
    Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>

--- plat/imx/imx8m/imx8mq/platform.mk
+++ plat/imx/imx8m/imx8mq/platform.mk
@@ -32,6 +32,8 @@ BL31_SOURCES		+=	plat/imx/common/imx8_helpers.S			\
 				plat/imx/imx8m/gpc_common.c			\
 				plat/imx/imx8m/imx_hab.c			\
 				plat/imx/imx8m/imx_aipstz.c			\
+				plat/imx/imx8m/imx_rdc.c			\
+				plat/imx/imx8m/imx8m_csu.c			\
 				plat/imx/imx8m/imx8m_caam.c			\
 				plat/imx/imx8m/imx8m_psci_common.c		\
 				plat/imx/imx8m/imx8mq/gpc.c			\
