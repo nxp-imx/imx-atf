723e49d6958654abafe0e3fee809d6c86f00c719
Peng Fan <peng.fan@nxp.com>
2020-07-05 21:59:53 +0800

    plat: imx8ulp: Add initial scmi upower pd support
    Add the initial upower and SCMI PD support
    
    upower API to version 48.11.10.5
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Signed-off-by: Jacky Bai <ping.bai@nxp.com>

---
+++ plat/imx/imx8ulp/upower/upower_hal.c
@@ -0,0 +1,184 @@
+/*
+ * Copyright 2020 NXP
+ *
+ * Peng Fan <peng.fan@nxp.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <common/debug.h>
+#include <drivers/delay_timer.h>
+#include <errno.h>
+#include <lib/mmio.h>
+
+#include "upower_soc_defs.h"
+#include "upower_api.h"
+#include "upower_defs.h"
+
+#define UPOWER_AP_MU1_ADDR	0x29280000
+
+extern void upwr_txrx_isr();
+
+struct MU_tag *muptr = (struct MU_tag *)UPOWER_AP_MU1_ADDR;
+
+void upower_apd_inst_isr()
+{
+	INFO("%s: entry\n", __func__);
+}
+
+uint32_t upower_status(int status)
+{
+    uint32_t ret = -1;
+    switch(status) {
+        case 0:
+            NOTICE("%s: finished successfully!\n", __func__);
+            ret = 0;
+            break;
+        case -1:
+            NOTICE("%s: memory allocation or resource failed!\n", __func__);
+            break;
+        case -2:
+            NOTICE("%s: invalid argument!\n", __func__);
+            break;
+        case -3:
+            NOTICE("%s: called in an invalid API state!\n", __func__);
+            break;
+        default:
+            NOTICE("%s: invalid return status\n", __func__);
+            break;
+    }
+    return ret;
+}
+
+
+void upower_wait_resp()
+{
+	while(muptr->SR.B.RFP == 0) {
+		NOTICE("%s: poll the mu:%x\n", __func__, muptr->SR.R);
+		udelay(100);
+	}
+	upwr_txrx_isr();
+}
+
+void user_upwr_rdy_callb(uint32_t soc, uint32_t vmajor, uint32_t vminor)
+{
+	NOTICE("%s: soc=%x\n", __func__, soc);
+	NOTICE("%s: RAM version:%d.%d\n", __func__, vmajor, vminor);
+}
+
+uint32_t upower_init(void)
+{
+	int status;
+
+	INFO("%s: entry\n",  __func__);
+
+	status = upwr_init(APD_DOMAIN, muptr, NULL, NULL, upower_apd_inst_isr, NULL);
+	if (upower_status(status)) {
+		NOTICE("%s: upower init failure\n", __func__);
+		return -EINVAL;
+	}
+
+        NOTICE("%s: start uPower RAM service\n", __func__);
+        status = upwr_start(1, user_upwr_rdy_callb);
+        upower_wait_resp();
+	/* poll status */
+        if (upower_status(status)) {
+            NOTICE("%s: upower init failure\n", __func__);
+	    return status;
+        }
+
+	/* Only for test */
+#if 1
+	uint32_t swt;
+	int ret_val, ret;
+
+	swt = BIT_32(1);
+
+	ret = upwr_pwm_power_off(&swt, NULL, NULL);
+	if (ret) {
+		NOTICE("%s failed: ret: %d\n", __func__, ret);
+		return ret;
+	}
+	upower_wait_resp();
+	ret = upwr_poll_req_status(UPWR_SG_PWRMGMT, NULL, NULL, &ret_val, 1000);
+	if (ret != UPWR_REQ_OK) {
+		NOTICE("Faliure %d, %s\n", ret, __func__);
+//		if (ret == UPWR_REQ_BUSY)
+//			return -EBUSY;
+//		else
+//			return -EINVAL;
+	}
+	NOTICE("=== %x\n", mmio_read_32(0x283590f4));
+
+	ret = upwr_pwm_power_on(&swt, NULL, NULL);
+	if (ret) {
+		NOTICE("%s failed: ret: %d\n", __func__, ret);
+		return ret;
+	}
+	upower_wait_resp();
+	ret = upwr_poll_req_status(UPWR_SG_PWRMGMT, NULL, NULL, &ret_val, 1000);
+	if (ret != UPWR_REQ_OK) {
+		NOTICE("Faliure %d, %s\n", ret, __func__);
+//		if (ret == UPWR_REQ_BUSY)
+//			return -EBUSY;
+//		else
+//			return -EINVAL;
+	}
+
+	NOTICE("=== %x\n", mmio_read_32(0x283590f4));
+
+	ret = upwr_pwm_power_off(&swt, NULL, NULL);
+	if (ret) {
+		NOTICE("%s failed: ret: %d\n", __func__, ret);
+		return ret;
+	}
+	upower_wait_resp();
+	ret = upwr_poll_req_status(UPWR_SG_PWRMGMT, NULL, NULL, &ret_val, 1000);
+	if (ret != UPWR_REQ_OK) {
+		NOTICE("Faliure %d, %s\n", ret, __func__);
+//		if (ret == UPWR_REQ_BUSY)
+//			return -EBUSY;
+//		else
+//			return -EINVAL;
+	}
+	NOTICE("=== %x\n", mmio_read_32(0x283590f4));
+#endif
+
+	return 0;
+}
+
+int upower_pwm(int domain_id, bool pwr_on)
+{
+	int ret, ret_val;
+	uint32_t swt;
+
+	if (domain_id == 9 || domain_id == 11 || domain_id == 12)
+		swt = BIT_32(12) | BIT_32(11) | BIT_32(10) | BIT_32(9);
+	else
+		swt = BIT_32(domain_id);
+	/*
+	 * TODO:
+	 *	Add domain_id check
+	 *	mem switch?
+	 */
+	if (pwr_on)
+		ret = upwr_pwm_power_on(&swt, NULL, NULL);
+	else
+		ret = upwr_pwm_power_off(&swt, NULL, NULL);
+
+	if (ret) {
+		NOTICE("%s failed: ret: %d, pwr_on: %d\n", __func__, ret, pwr_on);
+		return ret;
+	}
+	upower_wait_resp();
+	ret = upwr_poll_req_status(UPWR_SG_PWRMGMT, NULL, NULL, &ret_val, 1000);
+	if (ret != UPWR_REQ_OK) {
+		NOTICE("Faliure %d, %s\n", ret, __func__);
+		if (ret == UPWR_REQ_BUSY)
+			return -EBUSY;
+		else
+			return -EINVAL;
+	}
+
+	return 0;
+}
