12f028359ae9e3720e026b8b52b10ead71e0c27c
Jacky Bai <ping.bai@nxp.com>
2020-08-03 13:31:26 +0800

    MLK-24457 plat: imx8m: Fix the current fsp init
    The dfimisc reg value should be shift right 8 bit to
    get the current fsp.
    
    Signed-off-by: Jacky Bai <ping.bai@nxp.com>
    Reviewed-by: Anson Huang <Anson.Huang@nxp.com>

--- plat/imx/imx8m/ddr/dram.c
+++ plat/imx/imx8m/ddr/dram.c
@@ -179,7 +179,8 @@ void dram_info_init(unsigned long dram_timing_base)
 		DDRC_ACTIVE_TWO_RANK : DDRC_ACTIVE_ONE_RANK;
 
 	/* Get current fsp info */
-	current_fsp = mmio_read_32(DDRC_DFIMISC(0)) & 0xf;
+	current_fsp = mmio_read_32(DDRC_DFIMISC(0));
+	current_fsp = (current_fsp >> 8) & 0xf;
 	dram_info.boot_fsp = current_fsp;
 	dram_info.current_fsp = current_fsp;
 
@@ -225,7 +226,6 @@ void dram_info_init(unsigned long dram_timing_base)
 		ddr4_swffc(&dram_info, 0x0);
 	}
 }
-
 
 /*
  * For each freq return the following info:
