60fd986c04fd23239b750d980ec56accc52ad147
Shengjiu Wang <shengjiu.wang@nxp.com>
2020-12-08 18:45:48 +0800

    plat: imx8ulp: configure xrdc for hifi in LPAV
    imx8ulp: Move hifi configuration to separated function
    
    To support hifi reset, we need to move hifi configuration
    to separated function, then reconfigure them after reset.
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>
    Signed-off-by: Jacky Bai <ping.bai@nxp.com>
    Change-Id: Ib86d00403d6157faa3dc35a98a02ea64e5a06b68

--- plat/imx/common/imx_sip_handler.c
+++ plat/imx/common/imx_sip_handler.c
@@ -13,6 +13,7 @@
 #include <common/debug.h>
 #include <common/runtime_svc.h>
 #include <imx_sip_svc.h>
+#include <lib/mmio.h>
 #include <lib/el3_runtime/context_mgmt.h>
 #include <lib/mmio.h>
 #include <sci/sci.h>
@@ -288,3 +289,37 @@ int imx_kernel_entry_handler(uint32_t smc_fid,
 
 	return 0;
 }
+
+#if defined(PLAT_imx8ulp)
+int imx_hifi_xrdc(uint32_t smc_fid)
+{
+#define mmio_setbits32(addr, set)              mmio_write_32(addr, mmio_read_32(addr) | (set))
+#define mmio_clrbits32(addr, clear)            mmio_write_32(addr, mmio_read_32(addr) & ~(clear))
+	mmio_setbits32(0x2da50008, BIT_32(19) | BIT_32(17) | BIT_32(18));
+	mmio_clrbits32(0x2da50008, BIT_32(16));
+	printf("xxxxxx %x\n", mmio_read_32(0x2da50008));
+	extern int xrdc_config_mrc11_hifi_itcm(void);
+	extern int xrdc_config_mrc11_hifi_dtcm(void);
+	extern int xrdc_config_pdac(uint32_t, uint32_t, uint32_t, uint32_t);
+	extern int xrdc_config_mrc7_hifi_ddr(void);
+	extern int xrdc_config_mda(uint32_t mda_con, uint32_t dom);
+	xrdc_config_pdac(3, 47, 0, 7);
+	xrdc_config_mrc11_hifi_itcm();
+	xrdc_config_mrc11_hifi_dtcm();
+	xrdc_config_mda(9, 2);
+	xrdc_config_mrc7_hifi_ddr();
+
+	printf("mrc7 %x\n", mmio_read_32(0x292f2e00));
+	printf("mrc7 %x\n", mmio_read_32(0x292f2e04));
+	printf("mrc7 %x\n", mmio_read_32(0x292f2e08));
+	printf("mrc7 %x\n", mmio_read_32(0x292f2e0c));
+	printf("mrc7 %x\n", mmio_read_32(0x292f2e10));
+
+	printf("mda0 %x\n", mmio_read_32(0x292f0800));
+	printf("mda0 %x\n", mmio_read_32(0x292f0804));
+	printf("mda9 %x\n", mmio_read_32(0x292f0920));
+	printf("cr %x\n", mmio_read_32(0x292f0000));
+
+	return 0;
+}
+#endif
--- plat/imx/common/imx_sip_svc.c
+++ plat/imx/common/imx_sip_svc.c
@@ -33,6 +33,9 @@ static uintptr_t imx_sip_handler(unsigned int smc_fid,
 	case IMX_SIP_SCMI:
 		SMC_RET1(handle, scmi_handler(smc_fid, x1, x2, x3));
 		break;
+	case IMX_SIP_HIFI_XRDC:
+		SMC_RET1(handle, imx_hifi_xrdc(smc_fid));
+		break;
 #endif
 #if defined(PLAT_imx8mq)
 	case IMX_SIP_GET_SOC_INFO:
