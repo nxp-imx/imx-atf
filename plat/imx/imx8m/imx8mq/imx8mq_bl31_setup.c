f164bb061ecb17573f39614c3fb4ce3c7489f3af
Ye Li <ye.li@nxp.com>
2021-02-02 20:06:40 -0800

    MLK-25293 iMX8MQ: Add version for B2
    iMX8MQ B2 chip uses same OCOTP magic value with B1. So read the ROM
    version to distinguish it with B1.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Jacky Bai <ping.bai@nxp.com>
    (cherry picked from commit dcfce7731ed19c6235f9b1ad3a68fbfe33b69f9a)

--- plat/imx/imx8m/imx8mq/imx8mq_bl31_setup.c
+++ plat/imx/imx8m/imx8mq/imx8mq_bl31_setup.c
@@ -94,7 +94,10 @@ static void imx8mq_soc_info_init(void)
 	ocotp_val = mmio_read_32(IMX_OCOTP_BASE + OCOTP_SOC_INFO_B1);
 	if (ocotp_val == 0xff0055aa) {
 		imx_soc_revision &= ~0xff;
-		imx_soc_revision |= 0x21;
+		if (rom_version == 0x22)
+		    imx_soc_revision |= 0x22;
+		else
+		    imx_soc_revision |= 0x21;
 		return;
 	}
 }
