1c77e009cd569d221553ffa299f6575f38a5e16c
Jiafei Pan <Jiafei.Pan@nxp.com>
2021-02-05 16:44:15 +0800

    nxp: soc-errata: refine print information for erratas.
    Also print and list all DDR Erratas in order to check
    which erratas are implemented in current platform.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

--- plat/nxp/common/soc_errata/errata.c
+++ plat/nxp/common/soc_errata/errata.c
@@ -836,7 +836,7 @@ void erratum_a008850_post(void)
 	 */
 	mmio_write_32(cci_base + CTRL_OVERRIDE_REG, val);
 
-	INFO("SoC Errata A008850 Post was applied\n");
+	INFO("SoC workaround for Errata A008850 Post-Phase was applied\n");
 }
 #endif
 
@@ -868,19 +868,44 @@ static void erratum_a010539(void)
 void soc_errata(void)
 {
 #if ERRATA_SOC_A050426
-	INFO("SoC Errata A050426 was applied\n");
+	INFO("SoC workaround for Errata A050426 was applied\n");
 	erratum_a050426();
 #endif
 #if ERRATA_SOC_A008850
-	INFO("SoC Errata A008850 Early was applied\n");
+	INFO("SoC workaround for Errata A008850 Early-Phase was applied\n");
 	erratum_a008850_early();
 #endif
 #if ERRATA_SOC_A009660
-	INFO("SoC Errata A009660 was applied\n");
+	INFO("SoC workaround for Errata A009660 was applied\n");
 	erratum_a009660();
 #endif
 #if ERRATA_SOC_A010539
-	INFO("SoC Errata A010539 was applied\n");
+	INFO("SoC workaround for Errata A010539 was applied\n");
 	erratum_a010539();
+#endif
+	/*
+	 * The following DDR Erratas workaround are implemented in DDR driver,
+	 * but print information here.
+	 */
+#if ERRATA_DDR_A011396
+	INFO("SoC workaround for DDR Errata A011396 was applied\n");
+#endif
+#if ERRATA_DDR_A050450
+	INFO("SoC workaround for DDR Errata A050450 was applied\n");
+#endif
+#if ERRATA_DDR_A008511
+	INFO("SoC workaround for DDR Errata A008511 was applied\n");
+#endif
+#if ERRATA_DDR_A009803
+	INFO("SoC workaround for DDR Errata A009803 was applied\n");
+#endif
+#if ERRATA_DDR_A009942
+	INFO("SoC workaround for DDR Errata A009942 was applied\n");
+#endif
+#if ERRATA_DDR_A010165
+	INFO("SoC workaround for DDR Errata A010165 was applied\n");
+#endif
+#if ERRATA_DDR_A009663
+	INFO("SoC workaround for DDR Errata A009663 was applied\n");
 #endif
 }
