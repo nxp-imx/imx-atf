b1e7e4980e56cffaa18dadb428625d2fccfd665e
Biwen Li <biwen.li@nxp.com>
2021-03-11 12:02:21 +0800

    nxp: soc-ls1046a: remove robust definition of Boot Mode
    Remove robust definition of Boot Mode
    
    Signed-off-by: Biwen Li <biwen.li@nxp.com>

--- plat/nxp/soc-ls1046a/soc.mk
+++ plat/nxp/soc-ls1046a/soc.mk
@@ -64,24 +64,6 @@ $(eval $(call SET_FLAG,IMG_LOADR_NEEDED,BL2))
 $(eval $(call SET_FLAG,PSCI_NEEDED,BL31))
 $(eval $(call SET_FLAG,SIPSVC_NEEDED,BL31))
 
- # Selecting Boot Source for the TFA images.
-ifeq (${BOOT_MODE}, qspi)
-$(eval $(call SET_FLAG,QSPI_NEEDED,BL2))
-$(eval $(call add_define,QSPI_BOOT))
-else
-ifeq (${BOOT_MODE}, sd)
-$(eval $(call SET_FLAG,SD_MMC_NEEDED,BL2))
-$(eval $(call add_define,SD_BOOT))
-else
-ifeq (${BOOT_MODE}, emmc)
-$(eval $(call SET_FLAG,SD_MMC_NEEDED,BL2))
-$(eval $(call add_define,EMMC_BOOT))
-else
-$(error Un-supported Boot Mode = ${BOOT_MODE})
-endif
-endif
-endif
-
 # Source File Addition
 # #####################
 
