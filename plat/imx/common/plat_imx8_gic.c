9ebf721d2ee5a5cdaaf2d53fce26545a46ef571b
Nitin Garg <nitin.garg@nxp.com>
2021-03-18 12:39:03 -0500

    LF-3635: iMX8QM: Add support for CA72 and CA 53 Cockpit
    This change adds support for cockpit on each
    cluster of i.MX8QM. 2 indepedent OS can run on each
    clusters of i.MX8QM.
    
    Signed-off-by: Seb Fagard <sebastien.fagard@nxp.com>
    Signed-off-by: Fabrice Goucem <fabrice.goucem@nxp.com>
    Signed-off-by: Nitin Garg <nitin.garg@nxp.com>
    Signed-off-by: Abel Vesa <abel.vesa@nxp.com>

--- plat/imx/common/plat_imx8_gic.c
+++ plat/imx/common/plat_imx8_gic.c
@@ -7,6 +7,7 @@
 #include <platform_def.h>
 
 #include <common/bl_common.h>
+#include <common/debug.h>
 #include <common/interrupt_props.h>
 #include <drivers/arm/gicv3.h>
 #include <drivers/arm/arm_gicv3_common.h>
@@ -17,7 +18,11 @@
 #include <plat_imx8.h>
 
 /* the GICv3 driver only needs to be initialized in EL3 */
+#if ((defined COCKPIT_A72) || (defined COCKPIT_A53))
+uintptr_t rdistif_base_addrs[PLATFORM_GIC_CORE_COUNT];
+#else
 uintptr_t rdistif_base_addrs[PLATFORM_CORE_COUNT];
+#endif
 
 static const interrupt_prop_t g01s_interrupt_props[] = {
 	INTR_PROP_DESC(8, GIC_HIGHEST_SEC_PRIORITY,
@@ -30,7 +35,11 @@ static const interrupt_prop_t g01s_interrupt_props[] = {
 
 static unsigned int plat_imx_mpidr_to_core_pos(unsigned long mpidr)
 {
+#if (defined COCKPIT_A53) || (defined COCKPIT_A72)
+	return (unsigned int)plat_gic_core_pos_by_mpidr(mpidr);
+#else
 	return (unsigned int)plat_core_pos_by_mpidr(mpidr);
+#endif
 }
 
 const gicv3_driver_data_t arm_gic_data = {
@@ -38,7 +47,11 @@ const gicv3_driver_data_t arm_gic_data = {
 	.gicr_base = PLAT_GICR_BASE,
 	.interrupt_props = g01s_interrupt_props,
 	.interrupt_props_num = ARRAY_SIZE(g01s_interrupt_props),
+#if ((defined COCKPIT_A72) || (defined COCKPIT_A53))
+	.rdistif_num = PLATFORM_GIC_CORE_COUNT,
+#else
 	.rdistif_num = PLATFORM_CORE_COUNT,
+#endif
 	.rdistif_base_addrs = rdistif_base_addrs,
 	.mpidr_to_core_pos = plat_imx_mpidr_to_core_pos,
 };
@@ -77,30 +90,63 @@ static __inline void plat_gicr_exit_sleep(void)
 void plat_gic_init(void)
 {
 	plat_gicr_exit_sleep();
+#if (defined COCKPIT_A53) || (defined COCKPIT_A72)
+	/* check if GICD already configured, in case of 2nd partition (re)boot */
+	uint32_t gicd_ctlr = mmio_read_32(PLAT_GICD_BASE + GICD_CTLR);
+
+	if (gicd_ctlr & (CTLR_ENABLE_G0_BIT | CTLR_ENABLE_G1S_BIT | CTLR_ENABLE_G1NS_BIT)) {
+		NOTICE("GIC Distributor already configured: skip gicv3_distif_init \n");
+		/* clear all interrupts for this cluster */
+		gicv3_distif_clear();
+	} else {
+		gicv3_distif_init();
+		NOTICE("plat_gic_init: gicv3_distif_init done\n");
+	}
+	gicv3_distif_init();
+	gicv3_rdistif_init(plat_get_core_pos());
+	gicv3_cpuif_enable(plat_get_core_pos());
+#else
 	gicv3_distif_init();
 	gicv3_rdistif_init(plat_my_core_pos());
 	gicv3_cpuif_enable(plat_my_core_pos());
+#endif
 }
 
 void plat_gic_cpuif_enable(void)
 {
+#if (defined COCKPIT_A53) || (defined COCKPIT_A72)
+	gicv3_cpuif_enable(plat_get_core_pos());
+#else
 	gicv3_cpuif_enable(plat_my_core_pos());
+#endif
 }
 
 void plat_gic_cpuif_disable(void)
 {
+#if (defined COCKPIT_A53) || (defined COCKPIT_A72)
+	gicv3_cpuif_disable(plat_get_core_pos());
+#else
 	gicv3_cpuif_disable(plat_my_core_pos());
+#endif
 }
 
 void plat_gic_pcpu_init(void)
 {
+#if (defined COCKPIT_A53) || (defined COCKPIT_A72)
+	gicv3_rdistif_init(plat_get_core_pos());
+#else
 	gicv3_rdistif_init(plat_my_core_pos());
+#endif
 }
 
 void plat_gic_save(unsigned int proc_num, struct plat_gic_ctx *ctx)
 {
 	/* save the gic rdist/dist context */
+#if (defined COCKPIT_A53) || (defined COCKPIT_A72)
+	for (int i = 0; i < PLATFORM_GIC_CORE_COUNT; i++)
+#else
 	for (int i = 0; i < PLATFORM_CORE_COUNT; i++)
+#endif
 		gicv3_rdistif_save(i, &ctx->rdist_ctx[i]);
 	gicv3_distif_save(&ctx->dist_ctx);
 }
@@ -109,6 +155,10 @@ void plat_gic_restore(unsigned int proc_num, struct plat_gic_ctx *ctx)
 {
 	/* restore the gic rdist/dist context */
 	gicv3_distif_init_restore(&ctx->dist_ctx);
+#if (defined COCKPIT_A53) || (defined COCKPIT_A72)
+	for (int i = 0; i < PLATFORM_GIC_CORE_COUNT; i++)
+#else
 	for (int i = 0; i < PLATFORM_CORE_COUNT; i++)
+#endif
 		gicv3_rdistif_init_restore(i, &ctx->rdist_ctx[i]);
 }
