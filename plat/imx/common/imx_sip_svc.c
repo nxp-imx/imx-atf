2c85c48cf95cbd1700994c05f0b724ee3c7461ee
Peng Fan <peng.fan@nxp.com>
2021-06-17 16:49:35 +0800

    MLK-25575-6 imx8ulp: remove legacy scmi
    We will switch to use scmi framework, not use imx specific one.
    
    Reviewed-by: Jacky Bai <ping.bai@nxp.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- plat/imx/common/imx_sip_svc.c
+++ plat/imx/common/imx_sip_svc.c
@@ -30,9 +30,6 @@ static uintptr_t imx_sip_handler(unsigned int smc_fid,
 		SMC_RET1(handle, imx_kernel_entry_handler(smc_fid, x1, x2, x3, x4));
 		break;
 #if defined(PLAT_imx8ulp)
-	case IMX_SIP_SCMI:
-		SMC_RET1(handle, scmi_handler(smc_fid, x1, x2, x3));
-		break;
 	case IMX_SIP_HIFI_XRDC:
 		SMC_RET1(handle, imx_hifi_xrdc(smc_fid));
 		break;
