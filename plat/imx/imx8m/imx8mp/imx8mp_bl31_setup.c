e60edf11ca7367530ea07cd49b609c6532a69e18
Ji Luo <ji.luo@nxp.com>
2021-07-22 12:07:51 +0800

    MA-19291 imx8mn/imx8mp: Lock ocram secure access
    Lock the ocram secure access for imx8mn/imx8mp.
    
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Change-Id: Ida3bba565c0f94948b7e04c90075cd3352bbf6e9

--- plat/imx/imx8m/imx8mp/imx8mp_bl31_setup.c
+++ plat/imx/imx8m/imx8mp/imx8mp_bl31_setup.c
@@ -122,6 +122,7 @@ void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 		u_register_t arg2, u_register_t arg3)
 {
 	static console_t console;
+	unsigned int val;
 	unsigned int i;
 
 	/* Enable CSU NS access permission */
@@ -137,6 +138,8 @@ void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
 
 	/* config the ocram memory range for secure access */
 	mmio_write_32(IMX_IOMUX_GPR_BASE + 0x2c, 0x4E1);
+	val = mmio_read_32(IMX_IOMUX_GPR_BASE + 0x2c);
+	mmio_write_32(IMX_IOMUX_GPR_BASE + 0x2c, val | 0x3DFF0000);
 
 	imx8m_caam_init();
 
