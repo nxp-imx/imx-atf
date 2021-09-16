95dd4964ea96cf192f5808ab05f79dde13843aed
Jacky Bai <ping.bai@nxp.com>
2021-09-16 15:40:46 +0800

    LF-4723-03 plat: imx8ulp: Add system power off support
    On i.MX8ULP, we need to use the APD deep power down(DPD) mode
    to support the system power off function. when APD enter
    power off mode, only the RTD can re-kick it and boot from ROM.
    
    Signed-off-by: Jacky Bai <ping.bai@nxp.com>
    Reviewed-by: Ye Li <ye.li@nxp.com>

--- plat/imx/imx8ulp/imx8ulp_psci.c
+++ plat/imx/imx8ulp/imx8ulp_psci.c
@@ -120,6 +120,14 @@ void imx_pwr_domain_off(const psci_power_state_t *target_state)
 
 /* APD power mode config */
 ps_apd_pwr_mode_cfgs_t apd_pwr_mode_cfgs = {
+	[DPD_PWR_MODE] = {
+		.swt_board_offs = 0x180,
+		.swt_mem_offs = 0x188,
+		.pmic_cfg = PMIC_CFG(0x23, 0xa, 0x2),
+		.pad_cfg = PAD_CFG(0xc, 0x0, 0x01e80a02),
+		.bias_cfg = BIAS_CFG(0x0, 0x2, 0x2, 0x0),
+	},
+
 	/* PD */
 	[PD_PWR_MODE] = {
 		.swt_board_offs = 0x170,
@@ -148,6 +156,12 @@ ps_apd_pwr_mode_cfgs_t apd_pwr_mode_cfgs = {
 
 /* APD power switch config */
 ps_apd_swt_cfgs_t apd_swt_cfgs = {
+	[DPD_PWR_MODE] = {
+		.swt_board[0] = SWT_BOARD(0x00060003, 0x7c),
+		.swt_mem[0] = SWT_MEM(0x0, 0x0, 0x1ffff),
+		.swt_mem[1] = SWT_MEM(0x003fffff, 0x003fffff, 0x0),
+	},
+
 	[PD_PWR_MODE] = {
 		.swt_board[0] = SWT_BOARD(0x00060003, 0x00001e74),
 		.swt_mem[0] = SWT_MEM(0x00010c00, 0x0, 0x1ffff),
@@ -355,9 +369,44 @@ void imx_get_sys_suspend_power_state(psci_power_state_t *req_state)
 		req_state->pwr_domain_state[i] = PLAT_POWER_DOWN_OFF_STATE;
 }
 
-/* TODO */
 void __dead2 imx_system_off(void)
 {
+	int i;
+
+	/* config the all the core into OFF mode and IRQ masked. */
+	for (i = 0; i < PLATFORM_CORE_COUNT; i++) {
+		/* disable wakeup from wkpu */
+		mmio_write_32(IMX_SIM1_BASE + 0x3c + 0x4 * i, 0x0);
+
+		/* reset the core reset entry to 0x1000 */
+		imx_pwr_set_cpu_entry(i, 0x1000);
+
+		/* config the core power mode to off */
+		mmio_write_32(IMX_CMC1_BASE + 0x50 + 0x4 * i, 0x3);
+	}
+
+	plat_gic_cpuif_disable();
+
+	/* Config the power mode info for entering DPD mode and ACT mode */
+	imx_set_pwr_mode_cfg(ADMA_PWR_MODE);
+	imx_set_pwr_mode_cfg(ACT_PWR_MODE);
+	imx_set_pwr_mode_cfg(DPD_PWR_MODE);
+
+	/* Set the APD domain into DPD mode */
+	mmio_write_32(IMX_CMC1_BASE + 0x10, 0x7);
+	mmio_write_32(IMX_CMC1_BASE + 0x20, 0x1f);
+
+	/* make sure no pending upower wakeup */
+	upwr_xcp_set_rtd_apd_llwu(APD_DOMAIN, 0, NULL);
+	upower_wait_resp();
+
+	/* enable the upower wakeup from wuu, act as APD boot up method  */
+	mmio_write_32(IMX_PCC3_BASE + 0x98, 0xc0800000);
+	mmio_setbits_32(IMX_WUU1_BASE + 0x18, BIT(4));
+
+	/* make sure no pad wakeup event is pending */
+	mmio_write_32(IMX_WUU1_BASE + 0x20, 0xffffffff);
+
 	wfi();
 
 	ERROR("power off failed.\n");
