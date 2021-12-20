edba33e7e888d8a5000650bfdeb3fc76523b79e7
Ye Li <ye.li@nxp.com>
2021-12-20 12:30:28 +0800

    LF-5149 imx8ulp: Fix XRDC for audio SOF case
    When running audio SOC, HIFI4 DSP (DID2) needs to access SAI5
    and eDMA2, and eDMA2 (DID3) will transfer data between SAI5.
    
    So update the PDAC configuration for such case, allows SAI5 for
    DID2 and DID3.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Jacky Bai <ping.bai@nxp.com>

--- plat/imx/imx8ulp/xrdc/xrdc_config.h
+++ plat/imx/imx8ulp/xrdc/xrdc_config.h
@@ -89,6 +89,7 @@ struct xrdc_pac_msc_config imx8ulp_pdac[] = {
 	{ 0, 41, {0, 0, 0, 0, 0, 0, 7, 7} }, /* PAC0 slot 41 for SIM_AD */
 	{ 0, 47, {0, 0, 0, 0, 7, 0, 0, 7} }, /* PAC0 slot 47 for XRDC MGR*/
 	{ 1, PAC_SLOT_ALL, {0, 7, 0, 0, 0, 0, 0, 7} }, /* PAC1 */
+	{ 1, 9,  {0, 7, 7, 7, 0, 0, 0, 7} }, /* SAI5 for HIFI4 and eDMA2 */
 	{ 2, PAC_SLOT_ALL, {7, 0, 7, 7, 0, 0, 7, 7} }, /* PAC2 */
 };
 
