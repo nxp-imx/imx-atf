aa3d3fc9df9b9c6a257787f9f21d9af25184c4d7
Jiafei Pan <Jiafei.Pan@nxp.com>
2022-03-15 14:41:09 +0800

    feat(ls1012a): add ls1012a frwy_512mb board support
    Added support for ls1012a FRWY board with 512MB RAM.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

---
+++ plat/nxp/soc-ls1012a/ls1012afrwy_512mb/platform.mk
@@ -0,0 +1,25 @@
+#
+# Copyright 2018-2022 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+# board-specific build parameters
+BOOT_MODE	:=	qspi
+BOARD		:=	ls1012afrwy_512mb
+
+# DDR Compilation Configs
+DDRC_NUM_CS     :=      1
+
+# On-Board Flash Details
+QSPI_FLASH_SZ   :=      0x200000
+
+BL2_SOURCES	+=	${BOARD_PATH}/ddr_init.c
+
+SUPPORTED_BOOT_MODE	:=	qspi
+
+# Adding platform board build info
+include plat/nxp/common/plat_make_helper/plat_common_def.mk
+
+# Adding SoC build info
+include plat/nxp/soc-ls1012a/soc.mk
