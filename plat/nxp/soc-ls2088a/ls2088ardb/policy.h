7af461d463753e26ec7d9d91571bd080338be54d
Jiafei Pan <Jiafei.Pan@nxp.com>
2022-03-15 10:50:17 +0800

    feat(ls2088a): add ls2088ardb board support
    The LS2088A reference design board provides a comprehensive platform
    that enables design and evaluation of the product (LS2088A processor).
    
    Signed-off-by: Ruchika Gupta <ruchika.gupta@nxp.com>
    Signed-off-by: York Sun <york.sun@nxp.com>
    Signed-off-by: Pankaj Gupta <pankaj.gupta@nxp.com>
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

---
+++ plat/nxp/soc-ls2088a/ls2088ardb/policy.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2022 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef POLICY_H
+#define	POLICY_H
+
+/*
+ * Set this to 0x0 to leave the default SMMU page size in sACR
+ * Set this to 0x1 to change the SMMU page size to 64K
+ */
+#define POLICY_SMMU_PAGESZ_64K	0x1
+
+/*
+ * POLICY_PERF_WRIOP = 0 : No Performance enhancement for WRIOP RN-I
+ * POLICY_PERF_WRIOP = 1 : No Performance enhancement for WRIOP RN-I = 7
+ * POLICY_PERF_WRIOP = 2 : No Performance enhancement for WRIOP RN-I = 23
+ */
+#define POLICY_PERF_WRIOP	0
+
+#endif /* POLICY_H */
