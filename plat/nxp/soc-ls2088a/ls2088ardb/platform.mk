29e556e52db9a4045de401e85e85f0d8457a5582
Jiafei Pan <Jiafei.Pan@nxp.com>
2022-03-15 10:50:17 +0800

    feat(ls2088a): add ls2088ardb board support
    The LS2088A reference design board provides a comprehensive platform
    that enables design and evaluation of the product (LS2088A processor).
    
    Signed-off-by: Ruchika Gupta <ruchika.gupta@nxp.com>
    Signed-off-by: York Sun <york.sun@nxp.com>
    Signed-off-by: Pankaj Gupta <pankaj.gupta@nxp.com>
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>

---
+++ plat/nxp/soc-ls2088a/ls2088ardb/platform.mk
@@ -0,0 +1,40 @@
+#
+# Copyright 2022 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+# board-specific build parameters
+BOOT_MODE		?=	nor
+BOARD			:=	ls2088ardb
+POVDD_ENABLE		:=	no
+
+# DDR Compilation Configs
+CONFIG_DDR_NODIMM	:=	0
+NUM_OF_DDRC		:=	2
+DDRC_NUM_DIMM		:=	2
+DDRC_NUM_CS		:=	4
+DDR_ECC_EN		:=	yes
+CONFIG_STATIC_DDR 	:=	0
+
+# On-Board Flash Details
+# 128MB IFC NOR Flash
+NOR_FLASH_SZ		:=	0x8000000
+# 64MB QSPI Flash
+QSPI_FLASH_SZ		:=	0x4000000
+
+# Platform specific features.
+WARM_BOOT		:=	no
+
+# Adding Platform files build files
+BL2_SOURCES	+=	${BOARD_PATH}/ddr_init.c\
+			${BOARD_PATH}/platform.c
+
+SUPPORTED_BOOT_MODE	:=	nor	\
+				qspi
+
+# Adding platform board build info
+include plat/nxp/common/plat_make_helper/plat_common_def.mk
+
+# Adding SoC build info
+include plat/nxp/soc-ls2088a/soc.mk
