8705c609c9174ce5d84001429d8fb9752ad78455
Ji Luo <ji.luo@nxp.com>
2022-03-30 16:49:29 +0800

    MA-20186 imx8m: android: add secure configuration
    Add security configuration(CSU/RDC/TZASC) for android imx8m
    platforms.
    
    Change-Id: I76466500f129542a7405ee167251330ed1d770c4
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

--- plat/imx/imx8m/imx8mn/platform.mk
+++ plat/imx/imx8m/imx8mn/platform.mk
@@ -95,3 +95,7 @@ $(eval $(call add_define,IMX8M_DDR4_DVFS))
 ifeq (${SPD},trusty)
 	BL31_CFLAGS    +=      -DPLAT_XLAT_TABLES_DYNAMIC=1
 endif
+
+ifeq (${IMX_ANDROID_BUILD},true)
+$(eval $(call add_define,IMX_ANDROID_BUILD))
+endif
--- plat/imx/imx8m/imx8mq/platform.mk
+++ plat/imx/imx8m/imx8mq/platform.mk
@@ -95,3 +95,7 @@ $(eval $(call add_define,IMX_BOOT_UART_BASE))
 ifeq (${SPD},trusty)
 	BL31_CFLAGS    +=      -DPLAT_XLAT_TABLES_DYNAMIC=1
 endif
+
+ifeq (${IMX_ANDROID_BUILD},true)
+$(eval $(call add_define,IMX_ANDROID_BUILD))
+endif
