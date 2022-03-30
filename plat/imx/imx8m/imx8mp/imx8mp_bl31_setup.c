2c9dfada6f32a7fc80469c1b43f001b3a29cb94c
Ji Luo <ji.luo@nxp.com>
2022-03-30 16:49:29 +0800

    MA-20186 imx8m: android: add secure configuration
    Add security configuration(CSU/RDC/TZASC) for android imx8m
    platforms.
    
    Change-Id: I76466500f129542a7405ee167251330ed1d770c4
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

--- plat/imx/imx8m/imx8mp/imx8mp_bl31_setup.c
+++ plat/imx/imx8m/imx8mp/imx8mp_bl31_setup.c
@@ -47,6 +47,74 @@ static const struct aipstz_cfg aipstz[] = {
 	{0},
 };
 
+#ifdef IMX_ANDROID_BUILD
+static const struct imx_rdc_cfg rdc[] = {
+	/* Master domain assignment */
+	RDC_MDAn(RDC_MDA_M7, DID1),
+
+	RDC_MDAn(RDC_MDA_LCDIF, DID2),
+	RDC_MDAn(RDC_MDA_LCDIF2, DID2),
+	RDC_MDAn(RDC_MDA_HDMI_TX, DID2),
+
+
+	/* peripherals domain permission */
+	RDC_PDAPn(RDC_PDAP_UART2, D0R | D0W),
+	RDC_PDAPn(RDC_PDAP_WDOG1, D0R | D0W),
+	RDC_PDAPn(RDC_PDAP_RDC, D0R | D0W | D1R),
+
+	/* memory region */
+	RDC_MEM_REGIONn(40, 0x00000000, 0x50000000, ENA|D3R|D3W|D2R|/*D2W|*/D1R|D1W|D0R|D0W),
+	RDC_MEM_REGIONn(41, 0x50000000, 0x58000000, LCK|ENA|D3R|D3W|D2R|D2W|D1R|D1W|/*D0R|*/D0W),
+	RDC_MEM_REGIONn(42, 0x58000000, 0xFFFFFFFF, LCK|ENA|D3R|D3W|D2R|/*D2W|*/D1R|D1W|D0R|D0W),
+
+	/* Sentinel */
+	{0},
+};
+
+static const struct imx_csu_cfg csu_cfg[] = {
+	/* peripherals csl setting */
+	CSU_CSLx(CSU_CSL_OCRAM, CSU_SEC_LEVEL_2, LOCKED),
+	CSU_CSLx(CSU_CSL_OCRAM_S, CSU_SEC_LEVEL_2, LOCKED),
+	CSU_CSLx(CSU_CSL_RDC, CSU_SEC_LEVEL_3, LOCKED),
+	CSU_CSLx(CSU_CSL_TZASC, CSU_SEC_LEVEL_4, LOCKED),
+#ifdef SPD_trusty
+	CSU_CSLx(CSU_CSL_VPU, CSU_SEC_LEVEL_5, LOCKED),
+#endif
+
+	/* master HP0~1 */
+
+	/* SA setting */
+	CSU_SA(CSU_SA_M7, 1, LOCKED),
+	CSU_SA(CSU_SA_SDMA1, 1, LOCKED),
+	CSU_SA(CSU_SA_PCIE_CTRL1, 1, LOCKED),
+	CSU_SA(CSU_SA_USB1, 1, LOCKED),
+	CSU_SA(CSU_SA_USB2, 1, LOCKED),
+	CSU_SA(CSU_SA_ENET1, 1, LOCKED),
+	CSU_SA(CSU_SA_USDHC1, 1, LOCKED),
+	CSU_SA(CSU_SA_USDHC2, 1, LOCKED),
+	CSU_SA(CSU_SA_USDHC3, 1, LOCKED),
+	CSU_SA(CSU_SA_DAP, 1, LOCKED),
+	CSU_SA(CSU_SA_SDMA2, 1, LOCKED),
+	CSU_SA(CSU_SA_SDMA3, 1, LOCKED),
+	CSU_SA(CSU_SA_LCDIF1, 1, UNLOCKED),
+	CSU_SA(CSU_SA_ISI, 1, LOCKED),
+	CSU_SA(CSU_SA_LCDIF2, 1, UNLOCKED),
+	CSU_SA(CSU_SA_HDMI_TX, 1, LOCKED),
+	CSU_SA(CSU_SA_ENET2, 1, LOCKED),
+	CSU_SA(CSU_SA_GPU3D, 1, LOCKED),
+	CSU_SA(CSU_SA_GPU2D, 1, LOCKED),
+	CSU_SA(CSU_SA_VPU_G1, 1, LOCKED),
+	CSU_SA(CSU_SA_VPU_G2, 1, LOCKED),
+	CSU_SA(CSU_SA_VPU_VC8000E, 1, LOCKED),
+	CSU_SA(CSU_SA_ISP1, 1, LOCKED),
+	CSU_SA(CSU_SA_ISP2, 1, LOCKED),
+
+	/* HP control setting */
+
+	/* Sentinel */
+	{0}
+};
+#else
 static const struct imx_rdc_cfg rdc[] = {
 	/* Master domain assignment */
 	RDC_MDAn(RDC_MDA_M7, DID1),
@@ -76,6 +144,7 @@ static const struct imx_csu_cfg csu_cfg[] = {
 	/* Sentinel */
 	{0}
 };
+#endif
 
 static entry_point_info_t bl32_image_ep_info;
 static entry_point_info_t bl33_image_ep_info;
