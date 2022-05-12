8de0a60dfaa70948945a2619f2f2ddbde9b2f6a6
Jacky Bai <ping.bai@nxp.com>
2022-05-12 10:00:45 +0800

    MLK-25886-04 feat(imx93): enable the trdc init on imx93
    update the mmap region table for trdc init and enable
    the trdc init on imx93.
    
    Signed-off-by: Jacky Bai <ping.bai@nxp.com>
    Reviewed-by: Ye Li <ye.li@nxp.com>

--- plat/imx/imx93/platform.mk
+++ plat/imx/imx93/platform.mk
@@ -23,6 +23,7 @@ BL31_SOURCES		+=	plat/common/aarch64/crash_console_helpers.S   \
 				plat/imx/imx93/aarch64/plat_helpers.S		\
 				plat/imx/imx93/plat_topology.c			\
 				plat/imx/common/lpuart_console.S		\
+				plat/imx/imx93/trdc.c			\
 				plat/imx/imx93/imx93_bl31_setup.c		\
 				plat/imx/imx93/imx93_psci.c			\
 				lib/cpus/aarch64/cortex_a55.S			\
