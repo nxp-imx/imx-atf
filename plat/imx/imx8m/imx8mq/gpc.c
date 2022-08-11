54816ac81f3059c422898450aa863bdc7340e2a1
Zhipeng Wang <zhipeng.wang_1@nxp.com>
2022-08-11 16:26:10 +0800

    MA-20469 Enable suspend/resume when GPC interrupt-controller is disabled in the kernel
    Because ERR11171 and Android enable GKI, imx8mq cannot support cpuidle.
    In order to support suspend/resume on imx8mq board, remove the change
    of ERR11171 when IMX_ANDROID_BUILD=true. At this time, gpc monitors
    the wakeup irq and can wakeup, and is consistent with the logic of
    other imx8m boards.
    
    Change-Id: I0070ff59f26ea6694c28d5b0024471d24a721bc5
    Signed-off-by: Zhipeng Wang <zhipeng.wang_1@nxp.com>

--- plat/imx/imx8m/imx8mq/gpc.c
+++ plat/imx/imx8m/imx8mq/gpc.c
@@ -27,7 +27,9 @@
 #define FSL_SIP_CONFIG_GPC_SET_AFF	0x04
 #define FSL_SIP_CONFIG_GPC_CORE_WAKE	0x05
 
+#ifndef IMX_ANDROID_BUILD
 static uint32_t gpc_saved_imrs[16];
+#endif
 static uint32_t gpc_wake_irqs[4];
 static uint32_t gpc_imr_offset[] = {
 	IMX_GPC_BASE + IMR1_CORE0_A53,
@@ -56,6 +58,7 @@ static void gpc_imr_core_spin_unlock(unsigned int core_id)
 	spin_unlock(&gpc_imr_lock[core_id]);
 }
 
+#ifndef IMX_ANDROID_BUILD
 static void gpc_save_imr_lpm(unsigned int core_id, unsigned int imr_idx)
 {
 	uint32_t reg = gpc_imr_offset[core_id] + imr_idx * 4;
@@ -102,6 +105,7 @@ void imx_set_sys_wakeup(unsigned int last_core, bool pdn)
 			for (core = 0; core < 4; core++)
 				gpc_restore_imr_lpm(core, imr);
 }
+#endif
 
 static void imx_gpc_hwirq_mask(unsigned int hwirq)
 {
@@ -438,8 +442,10 @@ void imx_gpc_init(void)
 	for (i = 0; i < 4; i++)
 		mmio_write_32(gpc_imr_offset[i], ~0x1);
 
+#ifndef IMX_ANDROID_BUILD
 	/* leave the IOMUX_GPC bit 12 on for core wakeup */
 	mmio_setbits_32(IMX_IOMUX_GPR_BASE + 0x4, 1 << 12);
+#endif
 
 	/* use external IRQs to wakeup C0~C3 from LPM */
 	val = mmio_read_32(IMX_GPC_BASE + LPCR_A53_BSC);
