72274179141e3867a4944bd5ea1f2ebc61006ea8
Ye Li <ye.li@nxp.com>
2022-11-21 11:20:35 +0800

    LF-7819 imx8ulp: Fix suspend/resume issue when DBD owner is s400 only
    After resume from APD power down, XRDC is initialized by S400 but
    the PAC2 and MSC0-2 are not configured, so only DBD owner can access
    the resources.
    
    We have to move GPIO restore after ATF XRDC reinit and configure
    PDAC for PCC5 before enabling eDMA2 MP clock
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Jacky Bai <ping.bai@nxp.com>

--- plat/imx/imx8ulp/xrdc/xrdc_core.c
+++ plat/imx/imx8ulp/xrdc/xrdc_core.c
@@ -286,6 +286,9 @@ static int xrdc_apply_config(xrdc_check_func check_func)
 
 int xrdc_apply_lpav_config(void)
 {
+	/* Configure PAC2 to allow to access PCC5 */
+	xrdc_config_pac(2, 39, 0xe00000);
+
 	/* Enable the eDMA2 MP clock for MDA16 access */
 	mmio_write_32(0x2da70000, 0xc0000000);
 	return xrdc_apply_config(xrdc_check_lpav);
