a45e9678e69243e8cc54ab97a050e7629c63e160
Jacky Bai <ping.bai@nxp.com>
2022-12-14 15:14:03 +0800

    LF-7950-02 feat(imx93): enable mu1_b general interrupt 1
    when A55 cluster power down & ddr in retention, there is no good
    method to wakeup A55 cluster from low power mode by m33 side, so
    for the m33 low power mode demo, use the MU1_B general purpose
    interrupt 1 as a M33 controlled wakeup source to wakeup A55 cluster
    from low power mode.
    
    Signed-off-by: Jacky Bai <ping.bai@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- plat/imx/imx93/imx93_psci.c
+++ plat/imx/imx93/imx93_psci.c
@@ -39,6 +39,11 @@
 #define S400_MU_RRx(i)	(S400_MU_BASE + 0x280 + (i) * 4)
 #define ELE_POWER_DOWN_REQ	U(0x17d10306)
 
+#define MU1B_BASE	(0x44230000)
+#define MU1B_GIER	(MU1B_BASE + 0x110)
+#define MU1B_GSR	(MU1B_BASE + 0x118)
+#define MU_GPI1		BIT(1)
+
 #define CORE_PWR_STATE(state) ((state)->pwr_domain_state[MPIDR_AFFLVL0])
 #define CLUSTER_PWR_STATE(state) ((state)->pwr_domain_state[MPIDR_AFFLVL1])
 #define SYSTEM_PWR_STATE(state) ((state)->pwr_domain_state[PLAT_MAX_PWR_LVL])
@@ -156,10 +161,18 @@ void imx_set_sys_wakeup(unsigned int last_core, bool pdn)
 		 */
 		gpc_select_wakeup_raw_irq(CPU_A55C0);
 		gpc_select_wakeup_raw_irq(CPU_A55_PLAT);
+		/*
+		 * enable the MU1B general interrupt 1 for M33 SW to wakeup
+		 * A55 by assert an interrupt
+		 */
+		mmio_setbits_32(MU1B_GIER, MU_GPI1);
 	} else {
 		/* switch to GIC wakeup source for last_core and cluster */
 		gpc_select_wakeup_gic(CPU_A55C0);
 		gpc_select_wakeup_gic(CPU_A55_PLAT);
+		/* clear pending General interrupt 1 and disable the it */
+		mmio_clrbits_32(MU1B_GIER, MU_GPI1);
+		mmio_setbits_32(MU1B_GSR, MU_GPI1);
 	}
 
 	/* Set the GPC IMRs based on GIC IRQ mask setting */
