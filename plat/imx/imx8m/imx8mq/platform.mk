c95616958fb9cf232bf253d52afe7387e47f9ec2
Ji Luo <ji.luo@nxp.com>
2024-04-16 13:44:42 +0900

    MA-22318 imx8mq: disable dynamic xlat table
    Dynamic xlat table should not be enabled for Trusty or build
    break will happen due to ocram size limitation.
    
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

--- plat/imx/imx8m/imx8mq/platform.mk
+++ plat/imx/imx8m/imx8mq/platform.mk
@@ -104,10 +104,6 @@ ifeq (${IMX_BOOT_UART_BASE},auto)
 endif
 $(eval $(call add_define,IMX_BOOT_UART_BASE))
 
-ifeq (${SPD},trusty)
-	BL31_CFLAGS    +=      -DPLAT_XLAT_TABLES_DYNAMIC=1
-endif
-
 ifeq (${IMX_ANDROID_BUILD},true)
 $(eval $(call add_define,IMX_ANDROID_BUILD))
 endif
