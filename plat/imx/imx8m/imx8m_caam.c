a1cca91b8381fd02c2700a06ed58043e6ace9797
Jacky Bai <ping.bai@nxp.com>
2025-05-09 15:28:13 +0800

    LF-15212 fix(imx8m): add mask for jr0mid check
    In current code, if the CAAM_JR0MID is 0x8011, that means this Job Ring is
    reserved for HAB purpose. But In some conditions, the value read from the
    CAAM_JR0MID is 0x80008011, the LDID(bit31) will also be set to lock the
    register. In such case, th JR0 is also reserved for HAB. So use a value
    mask to only use the lower 16 bits to check if the CAAM JR0 is owned
    by secure world.
    
    Signed-off-by: Jacky Bai <ping.bai@nxp.com>
    Reviewed-by: Ye Li <ye.li@nxp.com>

--- plat/imx/imx8m/imx8m_caam.c
+++ plat/imx/imx8m/imx8m_caam.c
@@ -27,7 +27,7 @@ void imx8m_caam_init(void)
 	mmio_write_32(SM_CMD, sm_cmd);
 
 	/* config CAAM JRaMID set MID to Cortex A */
-	if (mmio_read_32(CAAM_JR0MID) == HAB_JR0_DID) {
+	if ((mmio_read_32(CAAM_JR0MID) & 0xffff) == HAB_JR0_DID) {
 		NOTICE("Do not release JR0 to NS as it can be used by HAB\n");
 	} else {
 		mmio_write_32(CAAM_JR0MID, CAAM_NS_MID);
