nxp-imx/imx-atf

commit bf2c4bfaac97358c5c2c7b4c6fc793166ece0389

LF-17689 fix(imx9): correct the netc WoL support
Fix the netc Wol support.

Reviewed-by: Ye Li <ye.li@nxp.com>
Signed-off-by: Jacky Bai <ping.bai@nxp.com>

plat/imx/imx9/common/imx9_sys_sleep.c
@@ -15,7 +15,7 @@
 #include <imx_scmi_client.h>
 #include <plat_imx8.h>
 
-#define IRQ_MASK(x)	irq_mask[(x) / 32U]
+#define IRQ_MASK(x)	((x) / 32U)
 #define IRQ_SHIFT(x)	(1U << (x) % 32U)
 
 static uint32_t irq_mask[IMR_NUM] = { 0x0 };
@@ -174,6 +174,10 @@ void imx_set_sys_wakeup(uint32_t last_core, bool pdn)
 {
 	uintptr_t gicd_base = PLAT_GICD_BASE;
 
+	/* Clear the wakeup and netc irq enabled flags */
+	has_wakeup_irq = false;
+	has_netc_irq = false;
+
 	/* Set the GPC IMRs based on GIC IRQ mask setting */
 	for (uint32_t i = 0U; i < IMR_NUM; i++) {
 		if (pdn) {
@@ -268,6 +272,4 @@ void imx9_sys_sleep_unprepare(uint32_t core_id)
 	}
 
 	imx_set_sys_wakeup(core_id, false);
-
-	has_wakeup_irq = false;
 }

plat/imx/imx9/common/imx9_psci_common.c
@@ -220,14 +220,14 @@ void imx_pwr_domain_suspend_finish(const psci_power_state_t *target_state)
 
 	/* system level */
 	if (is_local_state_off(SYSTEM_PWR_STATE(target_state))) {
-		imx9_sys_sleep_unprepare(core_id);
-
 		sys_mode = SCMI_IMX_SYS_POWER_STATE_MODE_MASK;
 		if (has_netc_irq) {
 			scmi_sys_pwr_state_set(imx9_scmi_handle,
 					       SCMI_SYS_PWR_FORCEFUL_REQ,
 					       sys_mode);
 		}
+
+		imx9_sys_sleep_unprepare(core_id);
 
 		/* switch to GIC wakeup source, config the target mode to WAIT */
 		scmi_core_set_sleep_mode(imx9_scmi_handle, SCMI_CPU_A55_ID(core_id),

commit 8bf8c1b1d95e60f7867a498f588b5b3b00bb9f68

LF-17844-01 fix(imx9): correct the active wakeup irq check logic
it is a regression caused by the IRQ_MASK macro update.
the irq_mask array should be used to check if an irq
is a valid wakeup irq.

Fixes: bf2c4bfaac97 ("LF-17689 fix(imx9): correct the netc WoL support")
Reviewed-by: Peng Fan <peng.fan@nxp.com>
Signed-off-by: Jacky Bai <ping.bai@nxp.com>

plat/imx/imx9/common/imx9_sys_sleep.c
@@ -144,7 +144,7 @@ static void wdog_restore(struct wdog_ctx *wdog)
 
 static inline bool active_wakeup_irq(uint32_t irq)
 {
-	return !(IRQ_MASK(irq) & IRQ_SHIFT(irq));
+	return !(irq_mask[IRQ_MASK(irq)] & IRQ_SHIFT(irq));
 }
 
 /*
@@ -158,6 +158,11 @@ static void peripheral_qchannel_hsk(bool en)
 	uint32_t num_hsks = 0U;
 
 	for (uint32_t i = 0U; i < ARRAY_SIZE(per_hsk_cfg); i++) {
+		/* We assume the per_hsk_cfg array valid entry ends if wakeup_irq = 0 */
+		if (per_hsk_cfg[i].wakeup_irq == 0U) {
+			break;
+		}
+
 		if (active_wakeup_irq(per_hsk_cfg[i].wakeup_irq)) {
 			per_lpm[num_hsks].perId = per_hsk_cfg[i].per_idx;
 			per_lpm[num_hsks].lpmSetting = en ? SCMI_CPU_PD_LPM_ON_RUN_WAIT_STOP :